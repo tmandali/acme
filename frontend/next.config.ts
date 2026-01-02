694e77db43602dcc7ce932bad8987036da505d91
TIMUR MANDALI <TIMUR.MANDALI+lcw@Lcwaikiki.com>
2026-01-02 14:04:10 +0300

    feat: Next.js Server Actions için izin verilen kaynakları yapılandır.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,8 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  // Allow development from other devices on the network
   experimental: {
     turbopackUseSystemTlsCerts: true,
+    serverActions: {
+      allowedOrigins: ["localhost:3000", "10.60.52.36:3000", "0.0.0.0:3000"]
+    }
   },
 };
 
