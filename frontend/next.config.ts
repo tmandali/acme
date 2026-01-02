1c21a5eaccb1ac672d3cb9825604b4eae342e849
timur-mandali_lcw <timur.mandali@lcwaikikiki.com>
2026-01-02 12:00:21 +0300

    feat: Add initial Next.js frontend setup, backend requirements, and new SQL/Python query definitions.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false
+  devIndicators: false,
+  experimental: {
+    turbopackUseSystemTlsCerts: true,
+  },
 };
 
 export default nextConfig;
